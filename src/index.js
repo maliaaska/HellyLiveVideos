1409a4e21ab2e9bf7b2e1a897a333e89699a2962
Arkadiusz <asmilewski@gmail.com>
2018-10-28 11:14:01 +0100

    video_list.js is mapping video

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
-
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAnZYyZ23_4bCbXwJUgfj6JWTEAnWrrTZM'; 
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  ); 
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos })
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    ); 
+  }
 } 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
