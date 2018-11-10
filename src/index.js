a549c35384e1393926d76d91c5bd6cb19223954f
Arkadiusz <asmilewski@gmail.com>
2018-11-10 11:35:26 +0100

    video_detail displays a one video with a description below

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAnZYyZ23_4bCbXwJUgfj6JWTEAnWrrTZM'; 
 
 
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail  video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     ); 
